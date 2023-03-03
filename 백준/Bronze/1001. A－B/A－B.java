dabf3ea913cb2736fb05851fb23545477f5b6aec
Kang Hyun Mo <55881475+jipgago@users.noreply.github.com>
2023-03-03 11:04:20 +0900

    [Bronze V] Title: A-B, Time: 132 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static int num1, num2;
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        num1 = Integer.parseInt(st.nextToken());
+        num2 = Integer.parseInt(st.nextToken());
+        System.out.println(num1 - num2);
+        
+    }
+}
